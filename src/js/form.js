0034b998de6739b95e9efbc2671c64801a6f28e6
Adam Liang <50341948+adamliang2019@users.noreply.github.com>
2019-09-25 21:45:28 -0700

    preemptive loading generate seeds and unique file name

--- src/js/form.js
+++ src/js/form.js
@@ -7,7 +7,12 @@ console.log(os.platform());
 var simRunning = false;
 var progress = 0;
 var numConditions;
+var numSeeds;
+var iterations;
+var minSeeds;
 var progressBar;
+var form;
+
 function execute(command, callback) {
     exec(command, (error, stdout, stderr) => { 
         callback(stdout); 
@@ -15,58 +20,113 @@ function execute(command, callback) {
 };
 
 (function() {
-    var run =  document.getElementById("run_button")
-    progressBar = document.getElementById("progress_bar")
-    run.addEventListener("click", function(){
-        // Collect the form data
-        document.getElementById("progress_bar_div").style.visibility = "visible";
-        form = document.getElementById("config_form");
-        var conditions = form.conditions.files[0].path;
-        var i;
-        var count = 0;
-        fs.createReadStream(conditions)
-            .on('data', function(chunk) {
-                for (i=0; i < chunk.length; ++i)
-                if (chunk[i] == 10) count++;
-            })
-            .on('end', function() {
-                numConditions = count;
+    var run_sim = document.getElementById("run_button");
+    var gen_seeds = document.getElementById("gen_seeds");
+    var conditions_file_element = document.getElementById("conditions_input");
+    var seeds_file_element = document.getElementById("seeds_input");
+    var conditions;
+    var seeds;
+    progressBar = document.getElementById("progress_bar");
+    form = document.getElementById("config_form");
+    
+    conditions_file_element.addEventListener("change", function(){  //calculate number of conditions
+        conditions = this.files[0].path;
+        numConditions = -1;
+        fs.createReadStream(conditions) 
+        .on('data', function(chunk) {
+            for (var i=0; i < chunk.length; ++i)
+                if (chunk[i] == 10) numConditions++;
+        })
+        .on('end', function(){
+            console.log("num conditions: "+numConditions );
         });
-        var iterations = form.iterations.value;
-        var seeds = form.seeds.files[0].path;
-        console.log(conditions)
-        console.log(iterations)
-        console.log(seeds)
-        let testFile = 'test_out.csv'
-        var fileToWatch = path.join(__dirname, '../',testFile);
+    });
+    
+    seeds_file_element.addEventListener("change", function(){       //calculate number of seeds in file
+        seeds = seeds_file_element.files[0].path;
+        numSeeds = 0;
+        fs.createReadStream(seeds)
+        .on('data', function(chunk) {
+            for (i=0; i < chunk.length; ++i)
+                if (chunk[i] == 10) numSeeds++;
+        })
+        .on('end', function(){
+            console.log("num seeds: "+numSeeds );
+        });
+    });
+    
+    gen_seeds.addEventListener("click", function(){                 //generate seeds file
+        if(!conditions_file_element.files[0] ||form.iterations.value==0){
+            console.log("please choose a conditions and seeds file\n");
+            return;
+        }
+        console.log("generating seeds\n");
+        iterations = form.iterations.value;
+        minSeeds = iterations * numConditions;
+        var time = new Date();
+        var date = time.toUTCString();
+        
+        let seedsFile = `seeds(${date}).csv`;
+        console.log(seedsFile);
+        var fileToWatch = path.join(__dirname, '../',seedsFile);
         // build command and run it 
-        const command = `./simulation ${iterations} ${conditions} ${seeds} >> ${testFile}`
-        exec(command, (err, output) => {
-            console.log(output);
-        });
-        // Set this flag so the file will be watched
-        simRunning=true;
-         //Watch the out file
-        fs.watchFile(fileToWatch, function (curr, prev) {
-            //console.log(simRunning)
-            if(simRunning){
-                var i;
-                var count = 0;
-                //Read current file and get how many lines
-                fs.createReadStream(fileToWatch)
-                .on('data', function(chunk) {
-                    for (i=0; i < chunk.length; ++i)
-                    if (chunk[i] == 10) count++;
-                })
-                .on('end', function() { // After reading update progress bar based on how many lines are there
-                    console.log(count);
-                    progress = Math.round((((count/2)+1)/numConditions)*100);
-                    progressBar.style.width = ""+progress+"%";
-                    progressBar.setAttribute('aria-valuenow', progress)
-                    progressBar.innerHTML =  ""+progress+"%";
-                    console.log(progress);
-                });
-            }
-        });
-      });
- })();
+        const command = `./seed_generator ${minSeeds+1} >> ${seedsFile}`
+        
+        function run_executable(cmd, callback) {
+            exec(cmd, (error, output) => {
+                console.log(output);
+                callback();
+            });
+        }
+        function finished(){
+            console.log("seeds generation finished\n");
+        }
+        run_executable(command, finished);
+    });
+        
+    run_sim.addEventListener("click", function(){                   //runs the simulation
+        console.log("starting simulation\n");
+        document.getElementById("progress_bar_div").style.visibility = "visible";
+        
+        iterations = form.iterations.value;
+        minSeeds = iterations * numConditions;
+        if (numSeeds>=minSeeds){
+            console.log(conditions)
+            console.log(iterations)
+            console.log(seeds)
+            let testFile = 'test_out.csv'
+            var fileToWatch = path.join(__dirname, '../',testFile);
+            // build command and run it 
+            const command = `./simulation ${iterations} ${conditions} ${seeds} >> ${testFile}`
+            exec(command, (err, output) => {
+                console.log(output);
+            });
+            // Set this flag so the file will be watched
+            simRunning=true;
+             //Watch the out file
+            fs.watchFile(fileToWatch, function (curr, prev) {
+                //console.log(simRunning)
+                if(simRunning){
+                    var i;
+                    var count = 0;
+                    //Read current file and get how many lines
+                    fs.createReadStream(fileToWatch)
+                    .on('data', function(chunk) {
+                        for (i=0; i < chunk.length; ++i)
+                        if (chunk[i] == 10) count++;
+                    })
+                    .on('end', function() { // After reading update progress bar based on how many lines are there
+                        console.log(count);
+                        progress = Math.round((((count/2)+1)/numConditions)*100);
+                        progressBar.style.width = ""+progress+"%";
+                        progressBar.setAttribute('aria-valuenow', progress)
+                        progressBar.innerHTML =  ""+progress+"%";
+                        console.log(progress);
+                    });
+                }
+            });
+        }else{
+            console.log("not enough seeds!\n");
+        }
+    });
+})();
